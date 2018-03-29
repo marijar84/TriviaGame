3d2fd665adf4e401f79c57cb20c8c2a1b8d48973
Maribel <maribel.jaramillo@vm2020.com>
2018-03-28 22:08:03 -0700

    Changes on TimeOut

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,15 @@
-
-
 $(document).ready(function () {
 
     var questionList = [];
-    var setTimeOutAnswer;
-    var interval = 3000;
 
-    var changeQuestion;
-    var intervalQuestion = 10000;
+    var setTimeOutAnswer;
+    var interval = 2000;
 
     var time;
     var intervalTime = 1000;
     var countTime = 13;
 
     var countQuestion = 0;
-
-    var i = 0;   
-    
-    changeQuestion = setInterval(drawQuestion, intervalQuestion);
-    time = setInterval(timeQuestion, intervalTime);
 
     //Data game
     function initializeData() {
@@ -27,14 +18,14 @@ $(document).ready(function () {
                 text: "The most populous city in the world is",
                 answer: "Tokyo",
                 options: ["Rusia", "Oslo", "Tokyo"],
-            },
+            }/*,
             {
                 text: "The language spoken by the people by Pakistan is",
                 answer: "Sindhi",
-                options: ["English", "Spanish", "Sindhi"],
+                options: ["English", "Sindhi", "Spanish"],
             },
             {
-                text: "TCountry that has the highest in Barley Production",
+                text: "Country that has the highest in Barley Production",
                 answer: "Russia",
                 options: ["USA", "China", "Russia"],
             },
@@ -42,115 +33,142 @@ $(document).ready(function () {
                 text: "Mount Everest is located in",
                 answer: "Nepal",
                 options: ["Nepal", "Bhutan", "New Delhi"],
-            },
-            {
-                text: "The most populous city in the world is",
-                answer: "Tokyo",
-                options: ["Rusia", "Oslo", "Copenhagen", "Tokio"],
-            },
-            {
-                text: "The most populous city in the world is",
-                answer: "Tokyo",
-                options: ["Rusia", "Oslo", "Copenhagen", "Tokio"],
-            },
-            {
-                text: "The most populous city in the world is",
-                answer: "Tokyo",
-                options: ["Rusia", "Oslo", "Copenhagen", "Tokio"],
-            },
-            {
-                text: "The most populous city in the world is",
-                answer: "Tokyo",
-                options: ["Rusia", "Oslo", "Copenhagen", "Tokio"],
-            },
-            {
-                text: "The most populous city in the world is",
-                answer: "Tokyo",
-                options: ["Rusia", "Oslo", "Copenhagen", "Tokio"],
-            }];
+            }*/];
     }
 
-    function timeQuestion(){
-        countTime--;
-        $("#time").text(countTime);
+    function timeQuestion() {
+        if (countQuestion != questionList.length) {
+            countTime--;
+            $("#time").text(countTime);
 
-        if(countTime = 0){
-            drawQuestion();
+            if (countTime == 1) {
+                visibleAnswer(false, true);
+            }
+        }
+        else{
+            clearInterval(time);
         }
     }
 
 
     function drawQuestion() {
-        var question = questionList[countQuestion].text;
-        var answer1 = questionList[countQuestion].options[0];
-        var answer2 = questionList[countQuestion].options[1];
-        var answer3 = questionList[countQuestion].options[2];
+        if (countQuestion == questionList.length) {
+            clearTimeout(time);
+            clearTimeout(setTimeOutAnswer);
+            time = null;
+            setTimeOutAnswer = null;
+            $("#container_start").css("visibility", "visible");
+            $("#container_question").css("visibility", "hidden");
 
-        $("#questionlabel").text(question);
-        $("#labelAsnwer1").text(answer1);
-        $("#customRadio1").attr("value", answer1);
-        $("#labelAsnwer2").text(answer2);
-        $("#customRadio2").attr("value", answer2);
-        $("#labelAsnwer3").text(answer3);
-        $("#customRadio3").attr("value", answer3); 
-        
-        countQuestion++;
+        } else {
+            var question = questionList[countQuestion].text;
+            var answer1 = questionList[countQuestion].options[0];
+            var answer2 = questionList[countQuestion].options[1];
+            var answer3 = questionList[countQuestion].options[2];
+
+            $("#questionlabel").text(question);
+            $("#labelAsnwer1").text(answer1);
+            $("#labelAsnwer1").css('background', '#ffffff');
+            $("#customRadio1").prop('checked', false);
+            $("#customRadio1").attr("value", answer1);
+
+            $("#labelAsnwer2").text(answer2);
+            $("#labelAsnwer2").css('background', '#ffffff');
+            $("#customRadio2").attr("value", answer2);
+            $("#customRadio2").prop('checked', false);
+
+            $("#labelAsnwer3").text(answer3);
+            $("#labelAsnwer3").css('background', '#ffffff');
+            $("#customRadio3").attr("value", answer3);
+            $("#customRadio3").prop('checked', false);
+        }
     }
-
-    function initialize() {
-        $("#time").text(countTime);
-        clearInterval(intervalAnswer);
-    }
-
-    initializeData();
-    drawQuestion();
 
     $("body").on('change', ".custom-radio", function (e) {
         var userSelected = e.target.value;
         var rightAnswer = questionList[countQuestion].answer;
 
-
-        console.log(userSelected);
-        console.log(rightAnswer);
-
-
         if (userSelected === questionList[countQuestion].answer) {
             console.log("Good Answer");
-            visibleAnswer(true);
+            visibleAnswer(true, false);
         }
         else {
             console.log("wrong answer");
-            visibleAnswer(false);
+            visibleAnswer(false, false);
         }
     });
 
-    function timeOutAnswer(isAnswer) {
-        console.log(i++);
-        if(isAnswer){
+    $("body").on('click', "#start", function (e) {
+        console.log("Question");
+        countQuestion = 0;
+        $("#container_question").css("visibility", "visible");
+        $("#container_start").css("visibility", "hidden");
 
-        }
-        else{
+        initializeData();
+        drawQuestion();
 
+        time = setTimeout(timeQuestion, intervalTime);
+    });
+
+    function rightAnswer() {
+        var label1 = $("#labelAsnwer1").text();
+        var label2 = $("#labelAsnwer2").text();
+        var label3 = $("#labelAsnwer3").text();
+
+        var answer = questionList[countQuestion].answer;
+
+        if (label1 === answer) {
+            $("#labelAsnwer1").css('background', '#00ab6f');
+        } else if (label2 === answer) {
+            $("#labelAsnwer2").css('background', '#00ab6f');
+        } else if (label3 === answer) {
+            $("#labelAsnwer3").css('background', '#00ab6f');
         }
-        $("#popup").css("visibility", "hidden");
-        clearInterval(setTimeOutAnswer);
     }
-   
 
-    function visibleAnswer(isAnswer) {
+    function nextAnswer() {
+
+        clearTimeout(setTimeOutAnswer);
+        setTimeOutAnswer = null;
+
+        countTime = 13;
+        $("#time").text = countTime;
+
+        $("#popup").css("visibility", "hidden");
+
+        countQuestion++;
+
+        drawQuestion();
+
+        time = setInterval(timeQuestion, intervalTime);
+    }
+
+
+    function visibleAnswer(isAnswer, isTimeOut) {
         console.log("Repeat");
         $("#popup").css("visibility", "visible");
 
-        setTimeOutAnswer = setInterval(timeOutAnswer, interval);
+        clearTimeout(time);
+        time = null;
 
-        if (isAnswer) {
-            $("#answerSelected").text("Congratulations");
-            $("#initialtitle").text("You choose the right answer");
-        }
-        else {
+        if (isTimeOut) {
             $("#answerSelected").text("Ooooppsss sorry!");
-            $("#initialtitle").text("Bad answer");
+            $("#initialtitle").text("Time is out");
+
+            rightAnswer();
+        } else {
+
+            if (isAnswer) {
+                $("#answerSelected").text("Congratulations!");
+                $("#initialtitle").text("You choose the right answer");
+            }
+            else {
+                $("#answerSelected").text("Ooooppsss sorry!");
+                $("#initialtitle").text("Bad answer");
+
+                rightAnswer();
+            }
         }
-        setTimeOutAnswer = setTimeout(timeOutAnswer, interval);
+        setTimeOutAnswer = setTimeout(nextAnswer, interval);
     }
 });
