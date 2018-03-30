25c20a2311938b6d56e3c70d0b2bc85319494e12
Maribel <maribel.jaramillo@vm2020.com>
2018-03-29 17:20:51 -0700

    Comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,20 @@
 $(document).ready(function () {
-
+    //Question List
     var questionList = [];
 
+    //Timeout when the user select an answer
     var setTimeOutAnswer;
     var interval = 3000;
 
+    //Timeout for time remaining
     var time;
     var intervalTime = 1000;
     var countTime = 13;
 
+    //To know what is the question
     var countQuestion = 0;
 
+    //To count when the user select correct or incorrect answer
     var countWin = 0;
     var countLoose = 0;
 
@@ -64,23 +68,28 @@ $(document).ready(function () {
             }];
     }
 
+    //Allows to count the time remaining
     function timeQuestion() {
-        if (countQuestion != questionList.length) {            
+        //If is different is because the user have not responded all questions
+        if (countQuestion != questionList.length) {
             countTime--;
             $("#time").text(countTime);
 
+            //If countTime is zero is because the time finished
             if (countTime == 1) {
                 countLoose++;
                 visibleAnswer(false, true);
             }
         }
-        else{
+        //The responded all question, and we have to stop timer
+        else {
             clearInterval(time);
         }
     }
 
-
+    //Allows to draw the question with answers
     function drawQuestion() {
+        //The user responded all question and we have to clean the timer and data
         if (countQuestion == questionList.length) {
             clearTimeout(time);
             clearTimeout(setTimeOutAnswer);
@@ -94,7 +103,10 @@ $(document).ready(function () {
             $("#incorrectAnswer").text("Incorrect Answer: " + countLoose);
             $("#result").css("visibility", "visible");
 
-        } else {
+        }
+        //Shows the user the next question
+        else 
+        {
             var question = questionList[countQuestion].text;
             var answer1 = questionList[countQuestion].options[0];
             var answer2 = questionList[countQuestion].options[1];
@@ -118,21 +130,30 @@ $(document).ready(function () {
         }
     }
 
+    //click event in radio button
     $("body").on('change', ".custom-radio", function (e) {
         var userSelected = e.target.value;
         var rightAnswer = questionList[countQuestion].answer;
 
+        //Validate is the user selected the right answer
         if (userSelected === questionList[countQuestion].answer) {
+            //Count win increment in 1, because the answer is correct
             countWin++;
+            //The first parameter is when the answer is correct
+            //The second parameter is when th time finished
             visibleAnswer(true, false);
         }
         else {
+            //Count loose increment in 1, because the answer is incorrect
             countLoose++;
+             //The first parameter is when the answer is incorrect
+            //The second parameter is when th time finished
             visibleAnswer(false, false);
         }
     });
 
     $("body").on('click', "#start", function (e) {
+        //Initialize data again because is th first time
         countQuestion = 0;
         countWin = 0;
         countLoose = 0;
@@ -147,13 +168,17 @@ $(document).ready(function () {
         time = setInterval(timeQuestion, intervalTime);
     });
 
+    //Allows to validate which is the correct answuer and show the user    
     function rightAnswer() {
+        //Get Text for each label
         var label1 = $("#labelAsnwer1").text();
         var label2 = $("#labelAsnwer2").text();
         var label3 = $("#labelAsnwer3").text();
 
+        //Get the answer 
         var answer = questionList[countQuestion].answer;
 
+        //Validate the answer and select the answer in html page
         if (label1 === answer) {
             $("#labelAsnwer1").css('background', '#00ab6f');
         } else if (label2 === answer) {
@@ -163,43 +188,54 @@ $(document).ready(function () {
         }
     }
 
+    //Allows to show the next question, this is happend when time finished
     function nextAnswer() {
 
+        //Clear popup timer
         clearTimeout(setTimeOutAnswer);
         setTimeOutAnswer = null;
 
+         //Hidde popup
+         $("#popup").css("visibility", "hidden");
+
+        //Set count in 13 again
         countTime = 13;
-        $("#time").text = countTime;
+        $("#time").text = countTime;       
 
-        $("#popup").css("visibility", "hidden");
-
+        //Increment in 1 to show the next question
         countQuestion++;
 
+        //Draw question
         drawQuestion();
 
+        //Clear and set question timer
         clearTimeout(time);
-
         time = setInterval(timeQuestion, intervalTime);
     }
 
-
+    //Validates when the user select the answer
     function visibleAnswer(isAnswer, isTimeOut) {
         $("#popup").css("visibility", "visible");
 
+        //Clear question timer
         clearTimeout(time);
         time = null;
 
+        //When the time finished
         if (isTimeOut) {
             $("#answerSelected").text("Ooooppsss sorry!");
             $("#initialtitle").text("Time is out");
 
+            //Show the correct answer
             rightAnswer();
         } else {
-
+            //When the user select answer
+            //Answer is correct, set text
             if (isAnswer) {
                 $("#answerSelected").text("Congratulations!");
                 $("#initialtitle").text("You choose the right answer");
             }
+            //Answer is incorrect, set text
             else {
                 $("#answerSelected").text("Ooooppsss sorry!");
                 $("#initialtitle").text("Bad answer");
